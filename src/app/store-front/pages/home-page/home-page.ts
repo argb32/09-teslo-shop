6ce15c46d133700943d47f18af34677ebc0af7ae
argb32 <usrext-alejandrorg@gmrcanarias.com>
2026-08-14 13:10:24 +0100

    add pagination functionality to home page

--- src/app/store-front/pages/home-page/home-page.ts
+++ src/app/store-front/pages/home-page/home-page.ts
@@ -1,8 +1,10 @@
 import { Component, inject } from '@angular/core';
 import { ProductCard } from '../../../products/components/product-card/product-card';
 import { ProductsService } from '../../../products/services/products.service';
-import { rxResource } from '@angular/core/rxjs-interop';
+import { rxResource, toSignal } from '@angular/core/rxjs-interop';
 import { Pagination } from "../../../shared/components/pagination/pagination";
+import { ActivatedRoute } from '@angular/router';
+import { map } from 'rxjs';
 
 @Component({
   selector: 'app-home-page',
@@ -13,10 +15,28 @@ export class HomePage {
 
   productService = inject(ProductsService);
 
+  activatedRoute = inject(ActivatedRoute);
+
+  currentPage = toSignal(
+    this.activatedRoute.queryParamMap.pipe(
+
+      map(params => params.get('page') ? +params.get('page')! : 1),
+      map(page => isNaN(page) ? 1 : page)
+    ),
+    {
+      initialValue: 1,
+    }
+  )
+
+
+
+
   productResource = rxResource({
-    params: () => ({}),
+    params: () => ({ page: this.currentPage() - 1 }),
     stream: ({ params }) => {
-      return this.productService.getProducts({})
+      return this.productService.getProducts({
+        offset: params.page * 9
+      })
     }
   })
 
