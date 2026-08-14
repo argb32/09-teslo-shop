d0d13ce916886f7171f4d6f083354a4cea5731bd
argb32 <usrext-alejandrorg@gmrcanarias.com>
2026-08-14 12:27:44 +0100

    products by gender

--- src/app/store-front/pages/gender-page/gender-page.ts
+++ src/app/store-front/pages/gender-page/gender-page.ts
@@ -1,9 +1,38 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { rxResource, toSignal } from '@angular/core/rxjs-interop';
+import { ActivatedRoute } from '@angular/router';
+import { map } from 'rxjs';
+import { ProductsService } from '../../../products/services/products.service';
+import { ProductCard } from "../../../products/components/product-card/product-card";
 
 @Component({
   selector: 'app-gender-page',
-  imports: [],
+  imports: [ProductCard],
   templateUrl: './gender-page.html',
   changeDetection: ChangeDetectionStrategy.Eager,
 })
-export class GenderPage {}
+export class GenderPage {
+  route = inject(ActivatedRoute);
+  productService = inject(ProductsService);
+
+  gender = toSignal<string>(
+    this.route.params.pipe(
+      //gender de la izquierda y el de la derecha no son exactamente lo mismo.
+      //this.route.params podría emitir:
+      //{
+      //   gender: 'men'
+      // }
+      map(({ gender }) => gender)
+    )
+  )
+
+
+  productResource = rxResource({
+    params: () => ({ gender: this.gender() }),
+    stream: ({ params }) => {
+      return this.productService.getProducts({
+        gender: params.gender
+      })
+    }
+  })
+}
