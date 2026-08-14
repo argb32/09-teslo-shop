cb8e1161359f75af251f81fd097e6ff4a898caac
argb32 <usrext-alejandrorg@gmrcanarias.com>
2026-08-14 09:30:08 +0100

    product cards with images

---
+++ src/app/products/pipes/product-image-pipe.ts
@@ -0,0 +1,19 @@
+import { Pipe, type PipeTransform } from '@angular/core';
+import { environment } from '../../../environments/environment';
+
+const BASE_URL = `${environment.baseUrl}/files/product`;
+
+@Pipe({
+  name: 'productImage',
+})
+export class ProductImagePipe implements PipeTransform {
+  transform(value: string | string[]): string {
+    if (typeof (value) === 'string') return `${BASE_URL}/${value}`;
+
+    if (!value.at(0)) return './assets/images/placeholder-images/no-image.jpg';
+
+    return `${BASE_URL}/${value.at(0)}`
+
+
+  }
+}
--- src/app/store-front/pages/product-page/product-page.ts
+++ src/app/store-front/pages/product-page/product-page.ts
@@ -1,4 +1,6 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, input } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ProductsService } from '../../../products/services/products.service';
 
 @Component({
   selector: 'app-product-page',
@@ -6,4 +8,14 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   templateUrl: './product-page.html',
   changeDetection: ChangeDetectionStrategy.Eager,
 })
-export class ProductPage {}
+export class ProductPage {
+
+  //para recibil la ruta necesitamos ActivatedRoute
+  activatedRoute = inject(ActivatedRoute);
+  productService = inject(ProductsService)
+
+  //snapshot porque no necesita ser dinamicao
+  productIdSlug = this.activatedRoute.snapshot
+
+
+}
