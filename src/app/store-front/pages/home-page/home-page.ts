921e2b43c706a51b20d851c85943211c5cabd28a
argb32 <usrext-alejandrorg@gmrcanarias.com>
2026-08-14 12:54:08 +0100

    pagination button (not functional)

--- src/app/store-front/pages/home-page/home-page.ts
+++ src/app/store-front/pages/home-page/home-page.ts
@@ -2,11 +2,12 @@ import { Component, inject } from '@angular/core';
 import { ProductCard } from '../../../products/components/product-card/product-card';
 import { ProductsService } from '../../../products/services/products.service';
 import { rxResource } from '@angular/core/rxjs-interop';
+import { Pagination } from "../../../shared/components/pagination/pagination";
 
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.html',
-  imports: [ProductCard],
+  imports: [ProductCard, Pagination],
 })
 export class HomePage {
 
