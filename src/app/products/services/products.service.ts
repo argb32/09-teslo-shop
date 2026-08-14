8c490674a918dd2e03f4617cb6dbc6b1f4766197
argb32 <usrext-alejandrorg@gmrcanarias.com>
2026-08-14 13:56:59 +0100

    save and load products from cache (super basic, not a real way to manage it)

--- src/app/products/services/products.service.ts
+++ src/app/products/services/products.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Service } from '@angular/core';
 import { Product, ProductsResponse } from '../interfaces/product.interface';
-import { Observable, tap } from 'rxjs';
+import { Observable, of, tap } from 'rxjs';
 import { environment } from '../../../environments/environment';
 
 const BASE_URL = environment.baseUrl
@@ -17,11 +17,18 @@ export class ProductsService {
 
   private http = inject(HttpClient);
 
+  private productsCache = new Map<string, ProductsResponse>();
+
 
   getProducts(options: Options): Observable<ProductsResponse> {
     //configuramos las opciones de la peticion por defecto si no vienen especificadas
     const { limit = 9, offset = 0, gender = '' } = options;
 
+    const key = `${limit}-${offset}-${gender}`;
+    //con esto buscamos si la key existe, y en caso afirmativo devolvemos los productos in hacer una nueva consulta a la api
+    if (this.productsCache.has(key)) {
+      return of(this.productsCache.get(key)!)
+    }
 
     return this.http.get<ProductsResponse>(`${BASE_URL}/products`, {
       params: {
@@ -30,7 +37,10 @@ export class ProductsService {
         gender,
       }
     })
-      .pipe(tap(resp => console.log(resp)));
+      .pipe(
+        tap(resp => console.log(resp)),
+        tap(resp => this.productsCache.set(key, resp))
+      );
 
 
   }
