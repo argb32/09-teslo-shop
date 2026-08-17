2a9cae121f6e8833ce11b09711c316944fe8a759
argb32 <usrext-alejandrorg@gmrcanarias.com>
2026-08-17 14:02:31 +0100

    admin dashboard skeleton

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -11,6 +11,11 @@ export const routes: Routes = [
     ]
   },
   {
+    path: 'admin',
+    loadChildren: () => import('./admin-dashboard/admin-dashboard.routes'),
+
+  },
+  {
     path: '',
     loadChildren: () => import('./store-front/store-front.routes')
   }
