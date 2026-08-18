b1121aeffbca2ef6cbe0c11b3cca393cb19b9698
argb32 <usrext-alejandrorg@gmrcanarias.com>
2026-08-18 08:52:43 +0100

    admin products list

--- src/app/admin-dashboard/pages/products-admin-page/products-admin-page.ts
+++ src/app/admin-dashboard/pages/products-admin-page/products-admin-page.ts
@@ -1,8 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { ProductTable } from "../../../products/components/product-table/product-table";
+import { rxResource } from '@angular/core/rxjs-interop';
+import { ProductsService } from '../../../products/services/products.service';
+import { PaginationService } from '../../../shared/components/pagination/pagination.service';
+import { Pagination } from "../../../shared/components/pagination/pagination";
 
 @Component({
   selector: 'app-products-admin-page',
-  imports: [],
+  imports: [ProductTable, Pagination],
   templateUrl: './products-admin-page.html',
 })
-export class ProductsAdminPage {}
+export class ProductsAdminPage {
+  productService = inject(ProductsService);
+  paginationService = inject(PaginationService);
+
+
+  productsPerPage = signal(10);
+
+
+  productResource = rxResource({
+    params: () => ({
+      page: this.paginationService.currentPage() - 1,
+      limit: this.productsPerPage()
+    }),
+    stream: ({ params }) => {
+      return this.productService.getProducts({
+        offset: params.page * 9,
+        limit: params.limit
+      })
+    }
+  })
+}
